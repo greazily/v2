403e537f9c940fc74e469a0ca7f0b343c77bec7c
full-bleed <dahlen.mikael@gmail.com>
2025-01-28 05:50:03 +0100

    not working on mobile or in firefox

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 gsap.registerPlugin(Draggable);
 
-const root = document.querySelector(":root"),
-    colors = ["cyan", "blue", "yellow", "green", "orange", "purple"],
-    circleElement = document.querySelector("#circleAnim"),
-    draggableElement = document.querySelector("#dragAnim"),
-    yearElements = document.querySelectorAll(".year"),
-    yearTextOffset = 7,
-    dialOffset = 90,
-    videoElements = document.querySelectorAll(".video-background"),
-    videoDuration = videoElements[0].duration * 0.5, // sortened !!!!!! be sure to remove !!!!!!!!
-    numberOfVideos = videoElements.length,  // Renamed variable here
-    totalDuration = videoDuration * numberOfVideos;  // Updated variable usage
+function initialize() {
+    const root = document.querySelector(":root"),
+        colors = ["white", "blue", "yellow", "green", "orange", "purple"],
+        circleElement = document.querySelector("#circleAnim"),
+        draggableElement = document.querySelector("#dragAnim"),
+        yearTextOffset = 7, // needs to be in relation to size of circle, maybe not since font is resized
+        dialOffset = 90,
+        videoElements = document.querySelectorAll(".video-background"),
+        videoDuration = videoElements[0].duration * 0.5, // sortened !!!!!! be sure to remove !!!!!!!!
+        numberOfVideos = videoElements.length,  // Renamed variable here
+        totalDuration = videoDuration * numberOfVideos;  // Updated variable usage
 let progressAtClick;
 
 function createYearElements() {
@@ -24,8 +24,6 @@ function createYearElements() {
     }
 }
 
-function initialize() {
-    console.log(videoDuration, numberOfVideos, totalDuration)
     function getCombinedProgress() {
         const rotation = gsap.getProperty(draggableElement, "rotation") % 360;
         const normalizedRotation = (rotation < 0) ? rotation + 360 : rotation;
@@ -44,19 +42,9 @@ function initialize() {
         }
     );
 
-    const rotationTimeline = gsap.to(circleElement, {
-        duration: totalDuration,
-        rotation: "360_cw",
-        repeat: -1,
-        ease: "none",
-        onUpdate: function () {
-            updateBackgroundColor(this.progress());
-        },
-    });
-
-    function updateBackgroundColor(progress) {
+    function updateVideoAndColor(progress) {
         videoTimeline.progress(progress);
-
+        // this might be the issue since "show" is being removed and add on every update, maybe not
         for (let i = 0; i < videoElements.length; i++) {
             if (progress > i / numberOfVideos && progress < (i + 1) / numberOfVideos) {  // Updated variable usage
                 root.style.setProperty("--color", colors[i])
@@ -67,6 +55,19 @@ function initialize() {
             }
         }
     }
+
+    const rotationTimeline = gsap.to(circleElement, {
+        duration: totalDuration,
+        rotation: "360_cw",
+        repeat: -1,
+        ease: "none",
+        onUpdate: function () {
+            updateVideoAndColor(this.progress());
+            // console.log(this.progress());
+            // videoTimeline.progress(this.progress());
+        },
+    });
+
 
     Draggable.create(draggableElement, {
         type: "rotation",
@@ -84,9 +85,10 @@ function initialize() {
             rotationTimeline.play();
         },
     });
+    createYearElements();
+    console.log(videoDuration, numberOfVideos, totalDuration)
 }
 
-window.onload = () => {
+window.onload = function() {
     initialize();
-    createYearElements();
 };
