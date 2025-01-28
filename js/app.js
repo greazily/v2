956aa19be8416fa1660fdedc28a9b4afd14c8789
full-bleed <dahlen.mikael@gmail.com>
2025-01-28 02:04:38 +0100

    cleaned up and added color changer

--- js/app.js
+++ js/app.js
@@ -1,99 +1,92 @@
 gsap.registerPlugin(Draggable);
-const c = document.querySelector("#circleAnim"),
-    r = document.querySelector("#dragAnim"),
-    yrs = document.querySelectorAll(".year"),
-    yrTxtOffset = 7,
+
+const root = document.querySelector(":root"),
+    colors = ["cyan", "blue", "yellow", "green", "orange", "purple"],
+    circleElement = document.querySelector("#circleAnim"),
+    draggableElement = document.querySelector("#dragAnim"),
+    yearElements = document.querySelectorAll(".year"),
+    yearTextOffset = 7,
     dialOffset = 90,
-    videos = document.querySelectorAll(".video-background"),
-    dVideo = videos[0].duration,
-    dv = videos.length,
-    d = dVideo * dv,
-    prjs = [1,2];
+    videoElements = document.querySelectorAll(".video-background"),
+    videoDuration = videoElements[0].duration * 0.5, // sortened !!!!!! be sure to remove !!!!!!!!
+    numberOfVideos = videoElements.length,  // Renamed variable here
+    totalDuration = videoDuration * numberOfVideos;  // Updated variable usage
 let progressAtClick;
 
-console.log(dv, d/dv)
-
-function placeYear(){
-    for (let i = 0; i < videos.length; i++) {
-        let yearElement = document.createElement("div");
-        let newR = -360/dv * (i) - dialOffset - yrTxtOffset;
-        yearElement.textContent = videos[i].dataset.year
-        yearElement.setAttribute("class", "year")
-        c.appendChild(yearElement)
-        yearElement.style.transform = "rotate(" + newR + "deg)"
-        console.log(videos[i].dataset.year)
-        console.log(newR)
-      }
+function createYearElements() {
+    for (let i = 0; i < videoElements.length; i++) {
+        const yearElement = document.createElement("div");
+        const rotationAngle = -360 / numberOfVideos * i - dialOffset - yearTextOffset;  // Updated variable usage
+        yearElement.textContent = videoElements[i].dataset.number;
+        yearElement.classList.add("year");
+        circleElement.appendChild(yearElement);
+        yearElement.style.transform = `rotate(${rotationAngle}deg)`;
+    }
 }
 
-function init(){
+function initialize() {
+    console.log(videoDuration, numberOfVideos, totalDuration)
     function getCombinedProgress() {
-        var rotation = gsap.getProperty(r, "rotation") % 360,
-            deg = (rotation < 0) ? rotation + 360 : rotation,
-            progressAtDeg = 1/360 * deg,
-            comProgress = progressAtDeg + progressAtClick;
-        return comProgress
+        const rotation = gsap.getProperty(draggableElement, "rotation") % 360;
+        const normalizedRotation = (rotation < 0) ? rotation + 360 : rotation;
+        const progressAtRotation = normalizedRotation / 360;
+        const combinedProgress = progressAtRotation + progressAtClick;
+        return combinedProgress;
     }
-    let tweenVideo = gsap.fromTo(".video-background", 
-        {
-            currentTime: 0,
-        },
-        {
-            duration: d/dv,
-            currentTime: dVideo || 1,
-            stagger: d/dv
-        },
-    )
 
-    let tween = gsap.to(c, {
-        duration: d,
+    const videoTimeline = gsap.fromTo(
+        ".video-background",
+        { currentTime: 0 },
+        {
+            duration: videoDuration,  // Updated variable usage
+            currentTime: videoElements[0].duration || 1,
+            stagger: videoDuration,  // Updated variable usage
+        }
+    );
+
+    const rotationTimeline = gsap.to(circleElement, {
+        duration: totalDuration,
         rotation: "360_cw",
         repeat: -1,
         ease: "none",
-        onUpdate: function(){
-            setBackgroundColor(this.progress())
-        }
-    })
+        onUpdate: function () {
+            updateBackgroundColor(this.progress());
+        },
+    });
 
-    function setBackgroundColor(progress){
-        tweenVideo.progress(progress)
+    function updateBackgroundColor(progress) {
+        videoTimeline.progress(progress);
 
-        for (let i = 0; i < videos.length; i++) {
-                    if(progress > i/dv && progress < (i+1)/dv){
-                        videos.forEach((video)=>{
-                            video.classList.remove("show")
-                        })
-                        videos[i].classList.add("show")
-                    }
+        for (let i = 0; i < videoElements.length; i++) {
+            if (progress > i / numberOfVideos && progress < (i + 1) / numberOfVideos) {  // Updated variable usage
+                root.style.setProperty("--color", colors[i])
+                videoElements.forEach((video) => {
+                    video.classList.remove("show");
+                });
+                videoElements[i].classList.add("show");
             }
+        }
     }
 
-    Draggable.create(r, {
+    Draggable.create(draggableElement, {
         type: "rotation",
-        onPress: function() {
-            progressAtClick = tween.progress();
-            tween.pause()
-            tweenVideo.pause()
-
+        onPress: function () {
+            progressAtClick = rotationTimeline.progress();
+            rotationTimeline.pause();
+            videoTimeline.pause();
         },
-        onDrag: function() {
-            tween.progress(getCombinedProgress())
-
+        onDrag: function () {
+            rotationTimeline.progress(getCombinedProgress());
         },
-        onRelease: function() {
-            tween.progress(getCombinedProgress())
-            gsap.set(r, {rotation: 0})
-            tween.play()
-        }
-    })
-
-
-
+        onRelease: function () {
+            rotationTimeline.progress(getCombinedProgress());
+            gsap.set(draggableElement, { rotation: 0 });
+            rotationTimeline.play();
+        },
+    });
 }
 
 window.onload = () => {
-    init();
-    placeYear();
-}
-
-
+    initialize();
+    createYearElements();
+};
