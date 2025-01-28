a056c642fbf9ffa63000f9d8a69ef84f491651f6
full-bleed <dahlen.mikael@gmail.com>
2025-01-28 17:27:32 +0100

    init

--- js/app.js
+++ js/app.js
@@ -1,95 +1,99 @@
 gsap.registerPlugin(Draggable);
 
-// !!!!!!!!!doesn't work on mobile or in firefox!!!!!!!!
-function initialize() {
-    const root = document.querySelector(":root"),
-        colors = ["white", "blue", "yellow", "green", "orange", "purple"],
-        circleElement = document.querySelector("#circleAnim"),
-        draggableElement = document.querySelector("#dragAnim"),
-        yearTextOffset = 7, // needs to be in relation to size of circle, maybe not since font is resized
-        dialOffset = 90,
-        videoElements = document.querySelectorAll(".video-background"),
-        videoDuration = videoElements[0].duration, // sortened !!!!!! be sure to remove !!!!!!!!
-        numberOfVideos = videoElements.length,  // Renamed variable here
-        totalDuration = videoDuration * numberOfVideos;  // Updated variable usage
-let progressAtClick;
+console.clear()
 
-function createYearElements() {
-    for (let i = 0; i < videoElements.length; i++) {
-        const yearElement = document.createElement("div");
-        const rotationAngle = -360 / numberOfVideos * i - dialOffset - yearTextOffset;  // Updated variable usage
-        yearElement.textContent = videoElements[i].dataset.number;
-        yearElement.classList.add("year");
-        circleElement.appendChild(yearElement);
-        yearElement.style.transform = `rotate(${rotationAngle}deg)`;
-    }
-}
-
-    function getCombinedProgress() {
-        const rotation = gsap.getProperty(draggableElement, "rotation") % 360;
-        const normalizedRotation = (rotation < 0) ? rotation + 360 : rotation;
-        const progressAtRotation = normalizedRotation / 360;
-        const combinedProgress = progressAtRotation + progressAtClick;
-        return combinedProgress;
-    }
-
-    const videoTimeline = gsap.fromTo(
-        ".video-background",
-        { currentTime: 0 },
-        {
-            duration: videoDuration,  // Updated variable usage
-            currentTime: videoElements[0].duration || 1,
-            stagger: videoDuration,  // Updated variable usage
-        }
-    );
-
-    function updateVideoAndColor(progress) {
-        videoTimeline.progress(progress);
-        // this might be the issue since "show" is being removed and add on every update, maybe not
-        for (let i = 0; i < videoElements.length; i++) {
-            if (progress > i / numberOfVideos && progress < (i + 1) / numberOfVideos) {  // Updated variable usage
-                root.style.setProperty("--color", colors[i])
-                videoElements.forEach((video) => {
-                    video.classList.remove("show");
-                });
-                videoElements[i].classList.add("show");
-            }
-        }
-    }
-
-    const rotationTimeline = gsap.to(circleElement, {
-        duration: totalDuration,
-        rotation: "360_cw",
-        repeat: -1,
-        ease: "none",
-        onUpdate: function () {
-            updateVideoAndColor(this.progress());
-            // console.log(this.progress());
-            // videoTimeline.progress(this.progress());
-        },
-    });
+let loadingValue = document.querySelector(".loading-value");
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+let dragProxy = document.querySelector(".proxy");
+let initialProgress = 0;
 
 
-    Draggable.create(draggableElement, {
-        type: "rotation",
-        onPress: function () {
-            progressAtClick = rotationTimeline.progress();
-            rotationTimeline.pause();
-            videoTimeline.pause();
-        },
-        onDrag: function () {
-            rotationTimeline.progress(getCombinedProgress());
-        },
-        onRelease: function () {
-            rotationTimeline.progress(getCombinedProgress());
-            gsap.set(draggableElement, { rotation: 0 });
-            rotationTimeline.play();
-        },
-    });
-    createYearElements();
-    console.log(videoDuration, numberOfVideos, totalDuration)
-}
+canvas.width = 1158;
+canvas.height = 770;
 
-window.onload = function() {
-    initialize();
+let frameCount = 147;
+let currentFrame = index => (
+  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${(index + 1).toString().padStart(4, '0')}.jpg`
+);
+
+let progression = 0.01
+let images = []
+let airpods = {
+  frame: 0
 };
+
+let imagesToLoad = frameCount;
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.onload = onLoad;
+  img.src = currentFrame(i);
+  images.push(img);
+}
+
+function getCombinedProgress(progressAtClick) {
+  let rotation = gsap.getProperty(dragProxy, "rotation") % 360;
+  let normalizedRotation = (rotation < 0) ? rotation + 360 : rotation;
+  let addedInitialRotation = normalizedRotation + progressAtClick;
+  let progressAtRotation = addedInitialRotation / 360;
+  return progressAtRotation ;
+
+}
+
+function onLoad() {
+  imagesToLoad--;
+  this.onload = null;  
+  loadingValue.textContent = Math.round((frameCount - imagesToLoad) / frameCount * 100) + "%"
+    
+  if (!imagesToLoad) {
+    render();
+    gsap.set(canvas, { autoAlpha: 1 })
+    gsap.to(".loading-container", { autoAlpha: 0 });    
+  }
+}
+
+let imageSequencer = gsap.to(airpods, {
+  frame: frameCount - 1,
+  snap: "frame",
+  duration: frameCount/30,
+  ease: "none",
+  onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
+});
+imageSequencer.pause()
+
+let dialRotater = gsap.to(".dial", {
+  duration: frameCount/30,
+  rotation: "360_cw",
+  repeat: -1,
+  ease: "none",
+  onUpdate: function(){
+    imageSequencer.progress(this.progress())
+  }
+})
+
+function getInitialProgress(){
+  let iP = gsap.getProperty(".dial", "rotation");
+  return iP
+}
+
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[airpods.frame], 0, 0); 
+}
+
+Draggable.create(dragProxy,{
+  type: "rotation",
+  onPress: function(){
+    dialRotater.pause()
+    initialProgress = getInitialProgress()
+  },
+  onDrag: function(){
+    dialRotater.progress(getCombinedProgress(initialProgress))
+  },
+  onRelease: function(){
+    gsap.set(dragProxy,{rotation: 0})
+    dialRotater.play()
+  }
+  
+})
