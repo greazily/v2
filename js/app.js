ec1576aa3abcb961ef068895197293a961cec356
full-bleed <dahlen.mikael@gmail.com>
2025-01-28 23:55:48 +0100

    image optimization 2nd round

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let loadingValue = document.querySelector(".loading-value");
 let canvas = document.querySelector("#canvas");
 let context = canvas.getContext("2d");
 let dragProxy = document.querySelector(".proxy");
-let frameRate = 24;
+let frameRate = 30;
 let framesPerProject = 0;   // use to get number of projects for creating project
 let initialProgress = 0;
 
@@ -50,52 +50,55 @@ function onLoad() {
     
   if (!imagesToLoad) {
     render();
-    gsap.set(canvas, { autoAlpha: 1 })
-    gsap.to(".loading-container", { autoAlpha: 0 });    
+    gsap.set(canvas, { autoAlpha: 1 });
+    gsap.to(".loading-container", { autoAlpha: 0 });
+    initializeAnimation();    
   }
 }
 
-let imageSequencer = gsap.to(airpods, {
-  frame: frameCount - 1,
-  snap: "frame",
-  duration: frameCount/frameRate,
-  ease: "none",
-  onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
-});
-imageSequencer.pause()
-
-let dialRotater = gsap.to(".dial", {
-  duration: frameCount/frameRate,
-  rotation: "360_cw",
-  repeat: -1,
-  ease: "none",
-  onUpdate: function(){
-    imageSequencer.progress(this.progress())
-  }
-})
-
-function getInitialProgress(){
-  let iP = gsap.getProperty(".dial", "rotation");
-  return iP
+function initializeAnimation(){
+    let imageSequencer = gsap.to(airpods, {
+      frame: frameCount - 1,
+      snap: "frame",
+      duration: frameCount/frameRate,
+      ease: "none",
+      onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
+    });
+    imageSequencer.pause()
+    
+    let dialRotater = gsap.to(".dial", {
+      duration: frameCount/frameRate,
+      rotation: "360_cw",
+      repeat: -1,
+      ease: "none",
+      onUpdate: function(){
+        imageSequencer.progress(this.progress())
+      }
+    })
+    
+    function getInitialProgress(){
+      let iP = gsap.getProperty(".dial", "rotation");
+      return iP
+    }
+    
+    Draggable.create(dragProxy,{
+      type: "rotation",
+      onPress: function(){
+        dialRotater.pause()
+        initialProgress = getInitialProgress()
+      },
+      onDrag: function(){
+        dialRotater.progress(getCombinedProgress(initialProgress))
+      },
+      onRelease: function(){
+        gsap.set(dragProxy,{rotation: 0})
+        dialRotater.play()
+      }
+    })
 }
 
 function render() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(images[airpods.frame], 0, 0); 
-}
-
-Draggable.create(dragProxy,{
-  type: "rotation",
-  onPress: function(){
-    dialRotater.pause()
-    initialProgress = getInitialProgress()
-  },
-  onDrag: function(){
-    dialRotater.progress(getCombinedProgress(initialProgress))
-  },
-  onRelease: function(){
-    gsap.set(dragProxy,{rotation: 0})
-    dialRotater.play()
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[airpods.frame], 0, 0); 
   }
-  
-})
+
