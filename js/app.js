30e5ff7ff92af06a5e6c6766e7b09e8bd4d5f493
full-bleed <dahlen.mikael@gmail.com>
2025-04-03 16:49:14 +0200

    on video load

--- js/app.js
+++ js/app.js
@@ -3,5 +3,18 @@ gsap.registerPlugin(Draggable);
 
 const vid = document.getElementById('video');
 
-console.log(vid.duration);
-// console.log(vid.currentTime);
+
+function onVideoLoad(){
+  vid.addEventListener('loadeddata', function() {
+    main();
+  }, false);
+};
+
+function main(){
+  
+  console.log(vid.duration, vid.currentTime);
+  
+
+};
+
+onVideoLoad();
