6c2dd9018c3dcc98acec2c37aa27e058c7c7fd57
full-bleed <dahlen.mikael@gmail.com>
2025-05-21 15:02:06 +0200

    init old js with new design

--- js/app.js
+++ js/app.js
@@ -1,125 +1,99 @@
 gsap.registerPlugin(Draggable);
 
+console.clear()
+
+let loadingValue = document.querySelector(".loading-value");
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+let dragProxy = document.querySelector(".proxy");
+let initialProgress = 0;
 
 
+canvas.width = 1024;
+canvas.height = 1024;
 
-function onVideoLoad(){
-  var video = document.getElementById('output');
-  video.src = 'vid/output-5k.mp4';
-  video.load();
-  video.addEventListener('loadeddata', function() {
-    main(video);
-  }, false);
+let frameCount = 966;
+let currentFrame = index => (
+  `/img/seq/${(index + 1).toString().padStart(4, '0')}.webp`
+);
+
+let progression = 0.01
+let images = []
+let airpods = {
+  frame: 0
 };
 
+let imagesToLoad = frameCount;
 
-function main(vid){
-  let 
-    dur = vid.duration,
-    storedRot = 0
-  ;
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.onload = onLoad;
+  img.src = currentFrame(i);
+  images.push(img);
+}
 
-  function infoTog(){
-    const 
-      sections = document.querySelectorAll('section'),
-      buttons = document.querySelectorAll('button')
-    ;
+function getCombinedProgress(progressAtClick) {
+  let rotation = gsap.getProperty(dragProxy, "rotation") % 360;
+  let normalizedRotation = (rotation < 0) ? rotation + 360 : rotation;
+  let addedInitialRotation = normalizedRotation + progressAtClick;
+  let progressAtRotation = addedInitialRotation / 360;
+  return progressAtRotation ;
 
-    buttons.forEach(button =>{
-      button.addEventListener('click', function (e) {
-        console.log(Array.prototype.indexOf.call(buttons, button))
-        let i = Array.prototype.indexOf.call(buttons, button)
-        if(i == 0) {
-          sections[0].classList.toggle('has-expanded')
-        } else {
-          sections[1].classList.toggle('has-expanded')
-        }
-      });
-    });
+}
 
+function onLoad() {
+  imagesToLoad--;
+  this.onload = null;  
+  loadingValue.textContent = Math.round((frameCount - imagesToLoad) / frameCount * 100) + "%";
+    
+  if (!imagesToLoad) {
+    render();
+    gsap.set(canvas, { autoAlpha: 1 });
+    gsap.to(".loading-container", { autoAlpha: 0 });    
+  }
+}
 
-  };
+let imageSequencer = gsap.to(airpods, {
+  frame: frameCount - 1,
+  snap: "frame",
+  duration: frameCount/30,
+  ease: "none",
+  onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
+});
+imageSequencer.pause()
 
+let dialRotater = gsap.to(".dial", {
+  duration: frameCount/30,
+  rotation: "360_cw",
+  repeat: -1,
+  ease: "none",
+  onUpdate: function(){
+    imageSequencer.progress(this.progress())
+  }
+})
 
-  function playVid(){
-    vid.play()
-  };
+function getInitialProgress(){
+  let iP = gsap.getProperty(".dial", "rotation");
+  return iP
+}
+
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[airpods.frame], 0, 0); 
+}
+
+Draggable.create(dragProxy,{
+  type: "rotation",
+  onPress: function(){
+    dialRotater.pause()
+    initialProgress = getInitialProgress()
+  },
+  onDrag: function(){
+    dialRotater.progress(getCombinedProgress(initialProgress))
+  },
+  onRelease: function(){
+    gsap.set(dragProxy,{rotation: 0})
+    dialRotater.play()
+  }
   
-  function resetVid(){
-    vid.currentTime = 0;
-  };
-  
-  function pauseVid(){
-    vid.pause()
-  };
-  
-  function resetProxy(){
-    gsap.set('#proxy',{rotation: 0})
-  };
-
-  function playRot(){
-    rotate.play()
-  };
-
-  function pauseRot(){
-    rotate.pause()
-  };
-
-  function dragRot(){
-    let combinedProg = (gsap.getProperty('#proxy', "rotation") + storedRot) % 360
-    combinedProg = (combinedProg < 0) ? combinedProg + 360 : combinedProg;
-    combinedProg = combinedProg / 360
-    return combinedProg;
-  };
-
-  function pressRot(){
-    let initialRot = 360 * rotate.progress();
-    return initialRot;
-  };
-
-  function storeRot(){
-    storedRot = pressRot()
-  };
-
-  function controlVidRot(){
-    rotate.progress(dragRot())
-    vid.currentTime = dur * dragRot()
-  };
-
-  let rotate = gsap.to('#dial', {
-    duration: dur,
-    rotation: "360_cw",
-    repeat: -1,
-    ease: "none",
-    onStart: function(){
-      playVid();
-      infoTog();
-    },
-    onRepeat: function(){
-      resetVid(); 
-      playVid()
-    }
-  });
-
-  Draggable.create("#proxy",{
-    type: "rotation",
-    onPress: function(){
-      pauseRot()
-      pauseVid()
-      storeRot()
-      
-    },
-    onDrag: function(){
-      controlVidRot()
-    },
-    onRelease: function(){
-      playRot()
-      playVid()
-      resetProxy()
-    }
-
-  });
-
-};
-
-onVideoLoad();
+})
