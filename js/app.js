4687fcac1e9370bac6ecf68129d95140dc76178d
full-bleed <dahlen.mikael@gmail.com>
2025-06-15 01:34:39 +0200

    reinitialize

--- js/app.js
+++ js/app.js
@@ -1,122 +1,46 @@
 gsap.registerPlugin(Draggable);
 
-console.clear()
+const indicator = document.getElementById("indicator");
+const header = document.getElementById("header");
+let headerHeight = header.getBoundingClientRect().height;
 
-let loadingValue = document.querySelector(".loading-value");
-let canvas = document.querySelector("#canvas");
-let context = canvas.getContext("2d");
-let dragProxy = document.querySelector(".proxy");
-let dial = document.querySelector(".dial");
-let titles = ["Hunterbrook", "Sony Group", "Data Desk", "The Dream Factory", "Degree Show", "Miscellaneous"]
-let initialProgress = 0;
-let videoLenghts = [0, 630, 1890, 2520, 3150, 3780];
-
-canvas.width = 1920;
-canvas.height = 1080;
-
-let frameCount = 5039;
-let currentFrame = index => (
-  `img/frm/${(index).toString().padStart(5, "0")}.webp`
-);
-
-let progression = 0.01
-let images = []
-let sequence = {
-  frame: 0
-};
-
-let imagesToLoad = frameCount;
-
-for (let i = 0; i < frameCount; i++) {
-  const img = new Image();
-  img.onload = onLoad;
-  img.src = currentFrame(i);
-  images.push(img);
-};
-
-
-
-let imageSequencer = gsap.to(sequence, {
-  frame: frameCount,
-  snap: "frame",
-  ease: "none",
-  paused: true,
-  onUpdate: render 
-});
-
-let dialRotater = gsap.to(dial, {
-  duration: frameCount/60,
-  top: "calc(100% - 1px)",
-  repeat: -1,
-  ease: "none",
-  paused: true,
-  onUpdate: function(){
-    imageSequencer.progress(this.progress());
-  }
-});
-
-function getInitialProgress(){
-  let iP = gsap.getProperty(dial, "rotation");
-  return iP
-};
-
-function render() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(images[sequence.frame], 0, 0); 
-}
-
-Draggable.create(dragProxy,{
-  type: "y",
-  bounds: ".drag-container",
-  onPress: function(){
-    dialRotater.pause();
+indicator.t1 = gsap.timeline({
+  onStart() {
+    console.log('start');
   },
-  onDrag: function() {
-    console.log(dial.style.top, dragProxy.style.transform)
-    // dial.style.top = ()
+  onUpdate() {
+    // console.log(this.progress())
   },
-  onRelease: function() {
-    dialRotater.play();
+  onComplete() {
+    this.restart()
+    console.log('complete');
   }
 })
+.to("#indicator", {
+  duration: 100,
+  y: headerHeight,
+  ease: "none"
+});
 
-function infoTog(){
-  const 
-    sections = document.querySelectorAll("section"),
-    buttons = document.querySelectorAll("button")
-  ;
+const proxy = document.createElement("div");
 
-  buttons.forEach(button =>{
-    button.addEventListener("click", function (e) {
-      console.log(Array.prototype.indexOf.call(buttons, button))
-      let i = Array.prototype.indexOf.call(buttons, button)
-      if(i == 0) {
-        sections[0].classList.toggle("has-expanded")
-      } else {
-        sections[1].classList.toggle("has-expanded")
-      }
+Draggable.create(proxy, {
+  type: 'y',
+  trigger: indicator,
+  onDragStart: function() {
+    indicator.t1.pause();
+  },
+  onDrag: function() {
+    indicator.t1.progress(this.y/headerHeight);
+  },
+  onDragEnd: function() {
+    indicator.t1.play();
+  },
+  onPress: function() {
+    gsap.set(this.target, {
+      y: indicator.t1.progress() * headerHeight
     });
-  });
-};
-
-
-function onLoad() {
-  imagesToLoad--;
-  this.onload = null;
-  let percent = Math.round((frameCount - imagesToLoad) / frameCount * 100 * 2)
-  if(percent <= 100){
-    loadingValue.textContent = percent + "%";
-  }
-  
-  if (percent == 100) {
-    infoTog();
-    gsap.set(canvas, { autoAlpha: 1 });
-    gsap.to(".loading-container", { 
-      autoAlpha: 0,
-      onComplete: function(){
-        render();
-        dialRotater.play();
-      }
-    });    
-  }
-}
+    this.update();
+  },
+  bounds: { minY: 0, maxY: headerHeight },
+});
