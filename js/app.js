21b6f33dc178a268eebffd35b33f842c7260bd66
full-bleed <dahlen.mikael@gmail.com>
2025-06-15 02:03:53 +0200

    app image sequence

--- js/app.js
+++ js/app.js
@@ -4,6 +4,34 @@ const indicator = document.getElementById("indicator");
 const header = document.getElementById("header");
 let headerHeight = header.getBoundingClientRect().height;
 
+let loadingValue = document.querySelector(".loading-value");
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+let videoLenghts = [0, 630, 1890, 2520, 3150, 3780];
+
+canvas.width = 1920;
+canvas.height = 1080;
+
+let frameCount = 5039;
+let currentFrame = index => (
+  `img/frm/${(index).toString().padStart(5, "0")}.webp`
+);
+
+let progression = 0.01
+let images = []
+let sequence = {
+  frame: 0
+};
+
+let imagesToLoad = frameCount;
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.onload = onLoad;
+  img.src = currentFrame(i);
+  images.push(img);
+};
+
 indicator.t1 = gsap.timeline({
   onStart() {
     console.log('start');
@@ -44,3 +72,25 @@ Draggable.create(proxy, {
   },
   bounds: { minY: 0, maxY: headerHeight },
 });
+
+
+function onLoad() {
+  imagesToLoad--;
+  this.onload = null;
+  let percent = Math.round((frameCount - imagesToLoad) / frameCount * 100 * 2)
+  if(percent <= 100){
+    loadingValue.textContent = percent + "%";
+  }
+  
+  if (percent == 100) {
+    infoTog();
+    gsap.set(canvas, { autoAlpha: 1 });
+    gsap.to(".loading-container", { 
+      autoAlpha: 0,
+      onComplete: function(){
+        render();
+        indicator.t1.play();
+      }
+    });    
+  }
+}
